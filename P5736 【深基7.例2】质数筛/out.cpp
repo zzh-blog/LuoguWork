ae699b139e083a9037a0fbb325132b53165d7337
Henry Zeng <z13405868569@163.com>
2020-07-25 17:09:01 +0800

    AC

--- P5736 【深基7.例2】质数筛/out.cpp
+++ P5736 【深基7.例2】质数筛/out.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n, tmp;
+bool prime_number(int n)
+{
+    if (n == 1)
+    {
+        return false;
+    } // 1不是质数
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        if (prime_number(tmp))
+        {
+            cout << tmp;
+            cout << " ";
+        }
+    }
+}
